16f81e9dec71519f9da4320fa5f21241d0d21abb
pbiecek <przemyslaw.biecek@gmail.com>
2015-01-28 00:21:31 +0100

    auta i auta_eng

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+auta2012_eng <- auta2012
+colnames(auta2012_eng) <- c("Price", "Currency", "Price.in.PLN", "Gross.Net", "HP", "kW",
+  "Brand", "Model", "Version", "No.doors", "Engine.cubic.capacity",
+  "Mileage", "To.fuel", "Year", "Color", "Country.of.current.registration",
+  "Country.of.origin", "Is.damaged", "Transmission", "Is.imported",
+  "Accessories")
