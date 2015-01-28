8c745a1c0552a0c02ecf15889c8d6ec6c20e2a5d
pbiecek <przemyslaw.biecek@gmail.com>
2015-01-28 11:06:03 +0100

    setNames()

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-auta2012_eng <- PogromcyDanych::auta2012
-colnames(auta2012_eng) <- c("Price", "Currency", "Price.in.PLN", "Gross.Net", "HP", "kW",
-  "Brand", "Model", "Version", "No.doors", "Engine.cubic.capacity",
-  "Mileage", "To.fuel", "Year", "Color", "Country.of.current.registration",
-  "Country.of.origin", "Is.damaged", "Transmission", "Is.imported",
-  "Accessories")
