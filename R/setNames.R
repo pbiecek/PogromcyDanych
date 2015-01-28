28c12f8ee5309fa66e6b8696ec9036a16260577f
pbiecek <przemyslaw.biecek@gmail.com>
2015-01-28 11:35:09 +0100

    global env

--- R/setNames.R
+++ R/setNames.R
@@ -2,10 +2,11 @@ setColnameLang <- function(lang = "eng") {
   if (lang == "eng") {
     auta2012_eng <- auta2012
     colnames(auta2012_eng) <- c("Price", "Currency", "Price.in.PLN", "Gross.Net", "HP", "kW",
-                                "Brand", "Model", "Version", "No.doors", "Engine.cubic.capacity",
-                                "Mileage", "To.fuel", "Year", "Color", "Country.of.current.registration",
+                                "Brand", "Model", "Version", "Nubmer.of.doors", "Engine.cubic.capacity",
+                                "Mileage", "Type.of.fuel", "Year", "Color", "Country.of.current.registration",
                                 "Country.of.origin", "Is.damaged", "Transmission", "Is.imported",
                                 "Accessories")
-    assign("auta2012", value = auta2012_eng, pos = -2)
+    assign("auta2012", value = auta2012_eng, envir = .GlobalEnv)
   }
+  invisible(0)
 }
