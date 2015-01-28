8f78d03516e3723fd42da322bbbf5e5e69585793
pbiecek <przemyslaw.biecek@gmail.com>
2015-01-28 11:47:02 +0100

    pol version

--- R/setNames.R
+++ R/setNames.R
@@ -1,12 +1,19 @@
 setColnameLang <- function(lang = "eng") {
+  auta2012_eng <- auta2012
   if (lang == "eng") {
-    auta2012_eng <- auta2012
     colnames(auta2012_eng) <- c("Price", "Currency", "Price.in.PLN", "Gross.Net", "HP", "kW",
                                 "Brand", "Model", "Version", "Nubmer.of.doors", "Engine.cubic.capacity",
                                 "Mileage", "Type.of.fuel", "Year", "Color", "Country.of.current.registration",
                                 "Country.of.origin", "Is.damaged", "Transmission", "Is.imported",
                                 "Accessories")
-    assign("auta2012", value = auta2012_eng, envir = .GlobalEnv)
+  } else {
+    colnames(auta2012_eng) <- c("Cena", "Waluta", "Cena.w.PLN", "Brutto.netto", "KM", "kW", 
+      "Marka", "Model", "Wersja", "Liczba.drzwi", "Pojemnosc.skokowa", 
+      "Przebieg.w.km", "Rodzaj.paliwa", "Rok.produkcji", "Kolor", "Kraj.aktualnej.rejestracji", 
+      "Kraj.pochodzenia", "Pojazd.uszkodzony", "Skrzynia.biegow", "Status.pojazdu.sprowadzonego", 
+      "Wyposazenie.dodatkowe")
+    
   }
+  assign("auta2012", value = auta2012_eng, envir = .GlobalEnv)
   invisible(0)
 }
